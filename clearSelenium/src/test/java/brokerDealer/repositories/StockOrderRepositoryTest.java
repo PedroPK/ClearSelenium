08f43866dfcde84108aed8313204c1afacd48d35
pedropk <pedropk@gmail.com>
2019-07-05 17:53:52 -0300

    Multiple orders saved Test

--- clearSelenium/src/test/java/brokerDealer/repositories/StockOrderRepositoryTest.java
+++ clearSelenium/src/test/java/brokerDealer/repositories/StockOrderRepositoryTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Optional;
 
@@ -69,10 +71,10 @@ public class StockOrderRepositoryTest {
 	}
 	
 	@Test
-	public void testSaveAndFlush() {
+	public void testFirstSaveAndFlush() {
 		// Arrange
 		StockOrder order = new StockOrder();
-		order.setDateTime(LocalTime.now());
+		order.setDateTime(LocalDateTime.now());
 		order.setTicker("PETR4F");
 		order.setQuantity(10);
 		order.setPrice(BigDecimal.valueOf(40));
@@ -91,10 +93,61 @@ public class StockOrderRepositoryTest {
 		assertFalse(findedOrder.isEmpty());
 		
 		logger.info("Original Order: " + order.toString());
-		//System.out.println(order);
-		
 		logger.info("Finded Order:   " + findedOrder.get().toString());
-		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSaveAndFlush_1Buy_3Sell() {
+		// Arrange
+		StockOrder orderBuyVVAR3 = new StockOrder();
+		orderBuyVVAR3.setDateTime(LocalDateTime.of(LocalDate.of(2019, 07, 02), LocalTime.of(10, 01, 02)));
+		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setQuantity(90);
+		orderBuyVVAR3.setPrice(5.13);
+		orderBuyVVAR3.setType(OrderType.BUY);
+		
+		StockOrder orderSellVVAR3_First = new StockOrder();
+		orderSellVVAR3_First.setDateTime(LocalDateTime.of(LocalDate.of(2019, 07, 03), LocalTime.of(11, 02, 03)));
+		orderSellVVAR3_First.setTicker("VVAR3");
+		orderSellVVAR3_First.setQuantity(45);
+		orderSellVVAR3_First.setPrice(5.5);
+		orderSellVVAR3_First.setType(OrderType.SELL);
+		
+		StockOrder orderSellVVAR3_Second = new StockOrder();
+		orderSellVVAR3_Second.setDateTime(LocalDateTime.of(LocalDate.of(2019, 07, 03), LocalTime.of(12, 03, 04)));
+		orderSellVVAR3_Second.setTicker("VVAR3");
+		orderSellVVAR3_Second.setQuantity(30);
+		orderSellVVAR3_Second.setPrice(5.9);
+		orderSellVVAR3_Second.setType(OrderType.SELL);
+		
+		StockOrder orderSellVVAR3_Third = new StockOrder();
+		orderSellVVAR3_Third.setDateTime(LocalDateTime.of(LocalDate.of(2019, 07, 05), LocalTime.of(12, 34, 35)));
+		orderSellVVAR3_Third.setTicker("VVAR3");
+		orderSellVVAR3_Third.setQuantity(55);
+		orderSellVVAR3_Third.setPrice(6.5);
+		orderSellVVAR3_Third.setType(OrderType.SELL);
+		
+		Example<StockOrder> orderBuyVVAR3Example = Example.of(orderBuyVVAR3);
+		
+		// Act
+		repository.saveAndFlush(orderBuyVVAR3);
+		repository.saveAndFlush(orderSellVVAR3_First);
+		repository.saveAndFlush(orderSellVVAR3_Second);
+		repository.saveAndFlush(orderSellVVAR3_Third);
+		
+		Optional<StockOrder> findedOrder = repository.findOne(orderBuyVVAR3Example);
+		
+		// Assert
+		assertNotNull(findedOrder);
+		assertTrue(findedOrder.isPresent());
+		assertFalse(findedOrder.isEmpty());
+		
+		logger.info("Buy VVAR3: 	" + orderBuyVVAR3.toString());
+		logger.info("Finded VAAR3:	" + findedOrder.get().toString());
+		
+		logger.info("Sell VVAR3 1: 	" + orderSellVVAR3_First.toString());
+		logger.info("Sell VVAR3 2: 	" + orderSellVVAR3_Second.toString());
+		logger.info("Sell VVAR3 3: 	" + orderSellVVAR3_Third.toString());
 	}
 	
 	@Ignore @Test
