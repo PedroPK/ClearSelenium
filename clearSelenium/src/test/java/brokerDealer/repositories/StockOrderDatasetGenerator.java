e47814376ca765a97e9ac76008d69eb0dc9c7959
pedropk <pedropk@gmail.com>
2019-07-21 17:19:37 -0300

    Constants for Tickers were extracted. New Tests were implemented.

--- clearSelenium/src/test/java/brokerDealer/repositories/StockOrderDatasetGenerator.java
+++ clearSelenium/src/test/java/brokerDealer/repositories/StockOrderDatasetGenerator.java
@@ -1,21 +1,24 @@
 package brokerDealer.repositories;
 
+import static brokerDealer.core.utils.DateTimeUtils.getLocalDateTime;
+
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Example;
 
-import static brokerDealer.core.utils.DateTimeUtils.*;
 import seleniumWebDriver.entities.StockOrder;
 import seleniumWebDriver.entities.enums.OrderType;
 
 public class StockOrderDatasetGenerator {
 	
-	public static List<StockOrder> get1Buy3SellVvar3StockOrders() {
+	private static final String BRFS3F_TICKER = "BRFS3F";
+	private static final String PETR4F_TICKER = "PETR4F";
+	private static final String VVAR3_TICKER = "VVAR3";
+	
+	public static List<StockOrder> get1Vvar3_1Buy3SellStockOrders() {
 		List<StockOrder> listStockOrdersToSaveAndFlush = new ArrayList<>();
 		
 		listStockOrdersToSaveAndFlush.add(getBuyVvar3StockOrder_2019_07_02());
@@ -30,7 +33,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder order = new StockOrder();
 		
 		order.setDateTime(LocalDateTime.now());
-		order.setTicker("PETR4F");
+		order.setTicker(PETR4F_TICKER);
 		order.setQuantity(10);
 		order.setPrice(BigDecimal.valueOf(40));
 		order.setType(OrderType.BUY);
@@ -65,7 +68,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2018, 10, 18, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(90);
 		orderBuyVVAR3.setPrice(5.13);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -77,7 +80,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2018, 11, 01, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(1);
 		orderBuyVVAR3.setPrice(16.63);
 		orderBuyVVAR3.setType(OrderType.SELL);
@@ -89,7 +92,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 05, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(10);
 		orderBuyVVAR3.setPrice(5.79);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -101,7 +104,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 8, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(1);
 		orderBuyVVAR3.setPrice(5.56);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -113,7 +116,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 14, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(10);
 		orderBuyVVAR3.setPrice(5.33);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -125,7 +128,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 15, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(9);
 		orderBuyVVAR3.setPrice(5.38);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -137,7 +140,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 19, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(10);
 		orderBuyVVAR3.setPrice(5.39);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -149,7 +152,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 21, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(5);
 		orderBuyVVAR3.setPrice(4.79);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -161,7 +164,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 21, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(5);
 		orderBuyVVAR3.setPrice(4.89);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -173,7 +176,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 25, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(10);
 		orderBuyVVAR3.setPrice(4.99);
 		orderBuyVVAR3.setType(OrderType.SELL);
@@ -185,7 +188,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 02, 26, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(10);
 		orderBuyVVAR3.setPrice(4.80);
 		orderBuyVVAR3.setType(OrderType.SELL);
@@ -197,7 +200,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderBuyVVAR3 = new StockOrder();
 		
 		orderBuyVVAR3.setDateTime(getLocalDateTime(2019, 07, 02, 10, 01, 02));
-		orderBuyVVAR3.setTicker("VVAR3");
+		orderBuyVVAR3.setTicker(VVAR3_TICKER);
 		orderBuyVVAR3.setQuantity(1);
 		orderBuyVVAR3.setPrice(13.50);
 		orderBuyVVAR3.setType(OrderType.BUY);
@@ -209,7 +212,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderSellVVAR3_First = new StockOrder();
 		
 		orderSellVVAR3_First.setDateTime(getLocalDateTime(2019, 07, 03, 11, 02, 03));
-		orderSellVVAR3_First.setTicker("VVAR3");
+		orderSellVVAR3_First.setTicker(VVAR3_TICKER);
 		orderSellVVAR3_First.setQuantity(45);
 		orderSellVVAR3_First.setPrice(5.5);
 		orderSellVVAR3_First.setType(OrderType.SELL);
@@ -221,7 +224,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderSellVVAR3_Second = new StockOrder();
 		
 		orderSellVVAR3_Second.setDateTime(getLocalDateTime(2019, 07, 03, 12, 03, 04));
-		orderSellVVAR3_Second.setTicker("VVAR3");
+		orderSellVVAR3_Second.setTicker(VVAR3_TICKER);
 		orderSellVVAR3_Second.setQuantity(30);
 		orderSellVVAR3_Second.setPrice(5.9);
 		orderSellVVAR3_Second.setType(OrderType.SELL);
@@ -233,7 +236,7 @@ public class StockOrderDatasetGenerator {
 		StockOrder orderSellVVAR3_Third = new StockOrder();
 		
 		orderSellVVAR3_Third.setDateTime(getLocalDateTime(2019, 07, 05, 12, 34, 35));
-		orderSellVVAR3_Third.setTicker("VVAR3");
+		orderSellVVAR3_Third.setTicker(VVAR3_TICKER);
 		orderSellVVAR3_Third.setQuantity(55);
 		orderSellVVAR3_Third.setPrice(6.5);
 		orderSellVVAR3_Third.setType(OrderType.SELL);
@@ -254,12 +257,63 @@ public class StockOrderDatasetGenerator {
 		return orderExample;
 	}
 	
-	public static StockOrder getBuyBrfs3StockOrder_2019_06_28() {
-		StockOrder orderBuyBrfs3 = new StockOrder();
-		orderBuyBrfs3.setDateTime(getLocalDateTime(2019, 06, 28, 10, 11, 12));
-		
+	private static StockOrder getBrfs3BuyStockOrder_2019_06_28() {
+		StockOrder orderBuyBrfs3 = 
+			new StockOrder(
+					2019, 06, 28, 10, 11, 12,
+					BRFS3F_TICKER,
+					30,
+					29.15,
+					OrderType.BUY);
 		
 		return orderBuyBrfs3;
+	}
+	
+	private static StockOrder getBrfs3SellStockOrder_2019_07_01() {
+		StockOrder orderBrfs3Sell = 
+			new StockOrder(
+					2019, 07, 01, 11, 12, 13,
+					BRFS3F_TICKER,
+					15,
+					31.10,
+					OrderType.SELL);
+		
+		return orderBrfs3Sell;
+	}
+	
+	private static StockOrder getBrfs3SellStockOrder_2019_07_02() {
+		StockOrder orderBrfs3Sell = 
+			new StockOrder(
+					2019, 07, 02, 12, 13, 14,
+					BRFS3F_TICKER,
+					10,
+					32.70,
+					OrderType.SELL);
+		
+		return orderBrfs3Sell;
+	}
+	
+	private static StockOrder getBrfs3SellStockOrder_2019_07_04() {
+		StockOrder orderBrfs3Sell = 
+			new StockOrder(
+					2019, 07, 04, 13, 14, 15,
+					BRFS3F_TICKER,
+					05,
+					34.50,
+					OrderType.SELL);
+		
+		return orderBrfs3Sell;
+	}
+	
+	public static List<StockOrder> getBrfs3_1Buy3SellsStockOrders() {
+		List<StockOrder>	listBrfs3StockOrders	=	new ArrayList<>();
+		
+		listBrfs3StockOrders.add(getBrfs3BuyStockOrder_2019_06_28());
+		listBrfs3StockOrders.add(getBrfs3SellStockOrder_2019_07_01());
+		listBrfs3StockOrders.add(getBrfs3SellStockOrder_2019_07_02());
+		listBrfs3StockOrders.add(getBrfs3SellStockOrder_2019_07_04());
+		
+		return listBrfs3StockOrders;
 	}
 	
 }
