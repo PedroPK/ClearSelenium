523705414a83836f09016a425cad514a4390bdb4
pedropk <pedropk@gmail.com>
2019-08-25 20:52:06 -0300

    New Methods to Sum Values of Buy and of Sell Orders.
    New Method to Calculate the Profit Percentage
    
    Added JavaDocs, indicating which methods have to be Tested yet

--- clearSelenium/src/main/java/brokerDealer/core/StockCalculator.java
+++ clearSelenium/src/main/java/brokerDealer/core/StockCalculator.java
@@ -3,8 +3,10 @@ package brokerDealer.core;
 import static brokerDealer.util.math.UtilMath.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -32,12 +34,12 @@ public class StockCalculator {
 		
 		if ( isListValid(pListStockOrder) ) {
 			for ( StockOrder stockOrder: pListStockOrder ) {
-				if ( stockOrder.getType() == OrderType.BUY ) {
+				if ( stockOrder.isBuyOrder() ) {
 					response = response.subtract(
 						stockOrder.getOrderValue()
 					);
 				} else {
-					if ( stockOrder.getType() == OrderType.SELL ) {
+					if ( stockOrder.isSellOrder() ) {
 						response = response.add(
 							stockOrder.getOrderValue()
 						);
@@ -99,12 +101,12 @@ public class StockCalculator {
 		return response;
 	}
 	
-	public static List<StockOrder> getBuyOrders( List<StockOrder> pListStockOrder ) {
+	public static List<StockOrder> getBuyOrders( List<StockOrder> pListStockOrders ) {
 		List<StockOrder> listBuyOrders = new ArrayList<>();
 		
-		if ( isListValid(pListStockOrder) ) {
-			for ( StockOrder stockOrder: pListStockOrder ) {
-				if ( stockOrder.getType() == OrderType.BUY ) {
+		if ( isListValid(pListStockOrders) ) {
+			for ( StockOrder stockOrder: pListStockOrders ) {
+				if ( stockOrder.isBuyOrder() ) {
 					listBuyOrders.add(stockOrder);
 				}
 			}
@@ -113,12 +115,35 @@ public class StockCalculator {
 		return listBuyOrders;
 	}
 	
+	
+	/**
+	 * TODO 
+	 * 		Create a test for this
+	 * 
+	 * 
+	 * @param pListStockOrders
+	 * @return
+	 */
+	public BigDecimal sumTotalValueBuyOrders( Optional<List<StockOrder>>	pListStockOrders	) {
+		BigDecimal result = BigDecimal.ZERO;
+		
+		if ( pListStockOrders.isPresent() ) {
+			for ( StockOrder stockOrder : pListStockOrders.get() ) {
+				if ( stockOrder.isBuyOrder() ) {
+					
+				}
+			}
+		}
+		
+		return result;
+	}
+	
 	public static List<StockOrder> getSellOrders( List<StockOrder> pListStockOrder ) {
 		List<StockOrder> listSellOrders = new ArrayList<>();
 		
 		if ( isListValid(pListStockOrder) ) {
 			for ( StockOrder stockOrder: pListStockOrder ) {
-				if ( stockOrder.getType() == OrderType.SELL ) {
+				if ( stockOrder.isSellOrder() ) {
 					listSellOrders.add(stockOrder);
 				}
 			}
@@ -127,33 +152,105 @@ public class StockCalculator {
 		return listSellOrders;
 	}
 	
+	/**
+	 * TODO		Implement Tests to this method
+	 * 
+	 * 
+	 * @param pListStockOrder
+	 * @return
+	 */
 	public static BigDecimal calcProfitPercentage( List<StockOrder> pListStockOrder ) {
-		BigDecimal response = BigDecimal.ZERO;
+		BigDecimal percentageOfProfit = BigDecimal.ZERO;
 		
 		if ( isListValid(pListStockOrder) ) {
-			for ( StockOrder stockOrder: pListStockOrder ) {
+			//for ( StockOrder stockOrder: pListStockOrder ) {
 				
-				int quantity = stockOrder.getQuantity();
+				// Sum Values from all Buy Orders
+				BigDecimal totalBuyValue = sumTotalValueFromBuyOrders(pListStockOrder);
+				
+				// Sum Values from all Sell Orders
+				BigDecimal totalSellValue = getTotalValueFromSellOrders(pListStockOrder);
+				
+				// Get the Division of Total Sell Value by the Total Buy Value
+				BigDecimal totalSellDividedByTotalBuyValues = totalSellValue.divide(totalBuyValue, 4,  RoundingMode.HALF_EVEN);
+
+				// Subtract 1 from this result
+				totalSellDividedByTotalBuyValues = totalSellDividedByTotalBuyValues.subtract(BigDecimal.ONE);
+				
+				// Multiply this to 100 to get the final Percentage of Profit
+				percentageOfProfit = totalSellDividedByTotalBuyValues.multiply(BigDecimal.TEN).multiply(BigDecimal.TEN);
+				/*
+				 * TODO 
+				 * 
+				 * Divide Sell by Buy values
+				 * 
+				 * Subtract by 1
+				 * 
+				 * Multiply by 100
+				 * 
+				 * This is the Percentage response
+				 */
+				
+				/*int quantity = stockOrder.getQuantity();
 				BigDecimal price = stockOrder.getPrice();
-				if ( stockOrder.getType() == OrderType.BUY ) {
+				if ( stockOrder.isBuyOrder() ) {
 					response = response.subtract(
 						multiply(price, quantity)
 					);
 				} else {
-					if ( stockOrder.getType() == OrderType.SELL ) {
+					if ( stockOrder.isSellOrder() ) {
 						response = response.add(
 							multiply(price, quantity)
 						);
 					}
-				}
+				}*/
 				
-				
-			}
+			//}
 		}
 		
-		return response;
+		return percentageOfProfit.setScale(2);
 	}
 	
+	/**
+	 * TODO		Implement Tests to this method
+	 * 
+	 * 
+	 * @param pListStockOrder
+	 * @return
+	 */
+	private static BigDecimal getTotalValueFromSellOrders(List<StockOrder> pListStockOrder) {
+		BigDecimal totalSellValue = BigDecimal.ZERO;
+		
+		List<StockOrder> sellOrders		= getSellOrders(pListStockOrder);
+		for (StockOrder sellStockOrder : sellOrders) {
+			totalSellValue = totalSellValue.add(sellStockOrder.getOrderValue());
+		}
+		return totalSellValue;
+	}
+	
+	/**
+	 * TODO		Implement Tests to this method
+	 * 
+	 * 
+	 * @param pListStockOrder
+	 * @return
+	 */
+	private static BigDecimal sumTotalValueFromBuyOrders(List<StockOrder> pListStockOrder) {
+		BigDecimal totalBuyValue = BigDecimal.ZERO;
+		List<StockOrder> buyOrders		= getBuyOrders(pListStockOrder);
+		for (StockOrder buyStockOrder : buyOrders) {
+			totalBuyValue = totalBuyValue.add(buyStockOrder.getOrderValue());
+		}
+		return totalBuyValue;
+	}
+	
+	/**
+	 * TODO		Implement Tests to this method
+	 * 
+	 * 
+	 * @param pListStockOrder
+	 * @return
+	 */
 	private static boolean isListValid(List<StockOrder> pListStockOrder) {
 		return pListStockOrder != null && !pListStockOrder.isEmpty();
 	}
