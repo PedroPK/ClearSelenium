7fa3294f3be1d7361cd67a67278a2929561ae329
pedropk <pedropk@gmail.com>
2019-10-26 13:22:48 -0300

    Isolating WebDriver handling in SeleniumUtils class

--- clearSelenium/src/test/java/clearSelenium/SeleniumUtils.java
+++ clearSelenium/src/test/java/clearSelenium/SeleniumUtils.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumUtils {
 	
 	private static final String CHROME_DRIVER_WINDOWS		=	"chromedriver.exe";
@@ -12,6 +17,19 @@ public class SeleniumUtils {
 	private static final String WEBDRIVER_CHROME_DRIVER		=	"webdriver.chrome.driver";
 	
 	private static final String OS_MAC_OS_X					=	"Mac OS X";
+	
+	/**
+	 * WebDriver instance
+	 */
+	private static WebDriver aWebDriver;
+	
+	public static WebDriver getWebDriver() {
+		if ( aWebDriver == null ) {
+			instanciateChromeDriver();
+		}
+		
+		return aWebDriver;
+	}
 	
 	public static void setSystemPropertyChromeWebDriverOriginal() {
 		String pathString = getWebDriverPath();
@@ -46,6 +64,62 @@ public class SeleniumUtils {
 		Path path = Paths.get("." + File.separator + "libs" + File.separator + "chromedriver");
 		String pathString = path.toString();
 		return pathString;
+	}
+	
+	public static WebDriver instanciateChromeDriver() {
+		SeleniumUtils.setSystemPropertyChromeWebDriverOriginal();
+		
+		if ( aWebDriver == null ) {
+			aWebDriver = new ChromeDriver();
+		}
+		
+		return aWebDriver;
+	}
+	
+	public static void accessURL(String pURL) {
+		getWebDriver().get(pURL);
+	}
+	
+	public static WebElement fillInputByXPath(String pXPath, String pValue) {
+		WebElement field = getWebDriver().findElement(By.xpath(pXPath));
+		
+		// Clear previously filled values
+		field.clear();
+		
+		// Fill the Field
+		field.sendKeys(pValue);
+		
+		return field;
+	}
+	
+	public static WebElement fillInputById(String pId, String pValue) {
+		WebElement field = getWebDriver().findElement(By.id(pId));
+		
+		// Clear previously filled values
+		field.clear();
+		
+		// Fill the Field
+		field.sendKeys(pValue);
+		
+		return field;
+	}
+	
+	public static WebElement getElementByXPath(String pXPath) {
+		WebElement field = getWebDriver().findElement(By.xpath(pXPath));
+		
+		return field;
+	}
+	
+	public WebElement getSpanByTickerContent(String pTicker) {
+		WebElement field = getElementByXPath("//*[contains(text(), '" + pTicker + "')]\"");
+		
+		return field;
+	}
+	
+	public static void closeWebDriver() {
+		if ( aWebDriver != null ) {
+			aWebDriver.close();
+		}
 	}
 	
 }
--- clearSelenium/src/test/java/clearSelenium/fundamentus/FundamentusHome.java
+++ clearSelenium/src/test/java/clearSelenium/fundamentus/FundamentusHome.java
@@ -1,19 +1,15 @@
 package clearSelenium.fundamentus;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import static clearSelenium.SeleniumUtils.accessURL;
+import static clearSelenium.SeleniumUtils.closeWebDriver;
+import static clearSelenium.SeleniumUtils.getElementByXPath;
+import static clearSelenium.SeleniumUtils.instanciateChromeDriver;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
-
-import clearSelenium.SeleniumUtils;
 
 public class FundamentusHome {
 	
@@ -22,14 +18,10 @@ public class FundamentusHome {
 	 */
 	private static final String URL_FUNDAMENTUS				= "https://www.fundamentus.com.br/index.php";
 	
-	private static final String PATH_CHROME_DRIVER			=	"C:\\Users\\pedro.carlos.santos\\git\\ClearSelenium\\clearSelenium\\src\\test\\resources\\chromedriver.exe";
-	
 	private static final String EXIBIR_BUTTON_XPATH			=	"/html/body/div[1]/div[1]/form/fieldset/input[2]";
 	
 	@FindBy(xpath=EXIBIR_BUTTON_XPATH)
 	private WebElement aExibirButton;
-	
-	private static WebDriver aWebDriver;
 	
 	/*
 	 * public FundamentusHome() { //instanciateChromeDriver(); }
@@ -39,66 +31,21 @@ public class FundamentusHome {
 	 * public FundamentusHome( WebDriver pWebDriver ) { aWebDriver = pWebDriver; }
 	 */
 	
-	public static FundamentusHome accessFundamentusHome() {
+	public static void accessFundamentusHome() {
 		instanciateChromeDriver();
 		
-		FundamentusHome fundamentusHome = new FundamentusHome();
-		FundamentusHome.accessURL();
-		
-		return fundamentusHome;
+		//FundamentusHome fundamentusHome = new FundamentusHome();
+		accessURL(URL_FUNDAMENTUS);
 	}
 	
 	@BeforeClass
-	public static void instanciateChromeDriver() {
-		SeleniumUtils.setSystemPropertyChromeWebDriverOriginal();
-		
-		aWebDriver = new ChromeDriver();
+	public static void initializeChromeDriver() {
+		instanciateChromeDriver();
 	}
 	
 	@Test
 	public void doNothingYet() {
 		accessFundamentusHome();
-	}
-
-	public static void accessURL() {
-		aWebDriver.get(URL_FUNDAMENTUS);
-	}
-	
-	private static WebElement fillInputByXPath(String pXPath, String pValue) {
-		WebElement field = aWebDriver.findElement(By.xpath(pXPath));
-		
-		// Clear previously filled values
-		field.clear();
-		
-		// Fill the Field
-		field.sendKeys(pValue);
-		
-		return field;
-	}
-	
-	private static WebElement fillInputById(String pId, String pValue) {
-		WebElement field = aWebDriver.findElement(By.id(pId));
-		
-		// Clear previously filled values
-		field.clear();
-		
-		// Fill the Field
-		field.sendKeys(pValue);
-		
-		return field;
-	}
-	
-	private static WebElement getElementByXPath(String pXPath) {
-		WebElement field = aWebDriver.findElement(By.xpath(pXPath));
-		
-		return field;
-	}
-	
-	
-	private WebElement getSpanByTickerContent(String pTicker) {
-		WebElement field = getElementByXPath("//*[contains(text(), '" + pTicker + "')]\"");
-		
-		return field;
 	}
 	
 	@Test
@@ -107,9 +54,7 @@ public class FundamentusHome {
 	}
 	
 	public FundamentusHome pressExibirButton() {
-		if ( aWebDriver == null ) {
 			accessFundamentusHome();
-		}
 		
 		if ( this.aExibirButton == null ) {
 			this.aExibirButton = getElementByXPath(EXIBIR_BUTTON_XPATH); 
@@ -123,9 +68,7 @@ public class FundamentusHome {
 	
 	@AfterClass
 	public static void close() {
-		if ( aWebDriver != null ) {
-			aWebDriver.close();
-		}
+		closeWebDriver();
 	}
 	
 }
