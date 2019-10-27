5aeb4e29f675ed754acfb94c7d54548639bf7fc8
pedropk <pedropk@gmail.com>
2019-10-27 16:00:38 -0300

    Working with a Map, with Ticker as Key and the Dividend Yield as Value

--- clearSelenium/src/test/java/clearSelenium/fundamentus/FundamentusHome.java
+++ clearSelenium/src/test/java/clearSelenium/fundamentus/FundamentusHome.java
@@ -6,10 +6,13 @@ import static clearSelenium.SeleniumUtils.getElementByXPath;
 import static clearSelenium.SeleniumUtils.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,8 +26,9 @@ public class FundamentusHome {
 	
 	private static final String EXIBIR_BUTTON_XPATH			=	"/html/body/div[1]/div[1]/form/fieldset/input[2]";
 	
-	private static final String TICKER_LINK_INDEX			=	"INDEX";
-	private static final String TICKER_LINK_XPATH			=	"//*[@id=\"test1\"]/tbody/tr[" + TICKER_LINK_INDEX + "]/td[1]/a";
+	private static final String TICKER_INDEX				=	"INDEX";
+	private static final String TICKER_LABEL_XPATH			=	"/html/body/div[1]/div[2]/div/div/table/tbody/tr[" + TICKER_INDEX + "]/td[1]/a";
+	private static final String TICKER_LINK_XPATH			=	"//*[@id=\"test1\"]/tbody/tr[" + TICKER_INDEX + "]/td[1]/a";
 	
 	private static final String DIVIDEND_YELD_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[9]/td[4]/span";
 	
@@ -51,16 +55,19 @@ public class FundamentusHome {
 		instanciateChromeDriver();
 	}
 	
+	@Ignore
 	@Test
 	public void doNothingYet() {
 		accessFundamentusHome();
 	}
 	
+	@Ignore
 	@Test
 	public void pressExibirButtonVoid() {
 		pressExibirButton();
 	}
 	
+	@Ignore
 	@Test
 	public void cliclFirstTickerLinks() {
 		pressExibirButton();
@@ -76,6 +83,7 @@ public class FundamentusHome {
 		firstTickerLink.click();
 	}
 	
+	@Ignore
 	@Test
 	public void getDividendYeldFromFirstTicker() {
 		pressExibirButton();
@@ -84,6 +92,7 @@ public class FundamentusHome {
 		String text = getDividendYeld();
 	}
 	
+	@Ignore
 	@Test
 	public void getDividendYeldFromSecondTicker() {
 		pressExibirButton();
@@ -105,24 +114,44 @@ public class FundamentusHome {
 	public void getDividendYeldsFrom3TickersInIteration() {
 		pressExibirButton();
 		
-		List<String> listDividendYelds = new ArrayList<>();
+		Map<String, String> mapDividendYelds = new HashMap<>();
 		for (int index = 1; index <= 3; index = index + 1) {
+			String tickerLabel = getTickerLabel(index);
 			openTickerPage(index);
 			
 			String dividendYeldTextValue = getDividendYeld();
 			
-			listDividendYelds.add(dividendYeldTextValue);
+			mapDividendYelds.put(
+				tickerLabel, 
+				dividendYeldTextValue);
 			
 			back();
 		}
+		
+		System.out.println(mapDividendYelds);
+	}
+
+	private String getTickerLabel(int pIndex) {
+		WebElement tickerLabelWebElement = getTickerLabelXPath(pIndex);
+		String tickerLabel = tickerLabelWebElement.getText();
+		return tickerLabel;
 	}
 	
 	private WebElement getTickerLinkXPath(int pTickerIndex) {
 		String tickerXPath = TICKER_LINK_XPATH;
 		
-		tickerXPath = tickerXPath.replace(TICKER_LINK_INDEX, pTickerIndex + "");
+		tickerXPath = tickerXPath.replace(TICKER_INDEX, pTickerIndex + "");
 		
 		WebElement firstTickerLink = getElementByXPath(tickerXPath);
+		return firstTickerLink;
+	}
+	
+	private WebElement getTickerLabelXPath(int pTickerIndex) {
+		String tickerLabelXPath = TICKER_LABEL_XPATH;
+		
+		tickerLabelXPath = tickerLabelXPath.replace(TICKER_INDEX, pTickerIndex + "");
+		
+		WebElement firstTickerLink = getElementByXPath(tickerLabelXPath);
 		return firstTickerLink;
 	}
 	
