fa1ed30e95414dfaf919986410f0bf05551b4674
pedropk <pedropk@gmail.com>
2019-11-03 18:16:57 -0300

    Oscillation Indicators
    Day Oscillation.
    
    Month Oscillation.
    
    30 Days Oscillation.
    
    12 Months Oscillation.
    
    Last Year Oscillation.
    
    Two Years ago Oscillation.
    
    Three Years ago Oscillation.
    
    Four Years ago Oscillation.
    
    Five Years ago Oscillation.

---
+++ clearSelenium/src/test/java/clearSelenium/fundamentus/OscilationIndicators.java
@@ -0,0 +1,56 @@
+package clearSelenium.fundamentus;
+
+public class OscilationIndicators {
+	
+	/**
+	 * Day Oscillation
+	 */
+	public static final String DAY_OSCILATION_XPATH					=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[2]/td[2]/span/font";
+	
+	/**
+	 * Month Oscillation
+	 */
+	public static final String MONTH_OSCILATION_XPATH				=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[3]/td[2]/span/font";
+	
+	/**
+	 * 30 Days Oscillation
+	 */
+	public static final String THRIRDY_DAY_OSCILATION_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[4]/td[2]/span/font";
+	
+	/**
+	 * 12 Month Oscillation
+	 */
+	public static final String TWELVE_MONTH_OSCILATION_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[5]/td[2]/span/font";
+	
+	/**
+	 * Present Year Oscillation
+	 */
+	public static final String PRESENT_YEAR_OSCILATION_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[6]/td[2]/span/font";
+	
+	/**
+	 * Last Year Oscillation
+	 */
+	public static final String LAST_YEAR_OSCILATION_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[7]/td[2]/span/font";
+	
+	/**
+	 * Two Year Oscillation
+	 */
+	public static final String TWO_YEAR_OSCILATION_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[8]/td[2]/span/font";
+	
+	/**
+	 * Three Years ago Oscillation
+	 */
+	public static final String THREE_YEARS_AGO_OSCILATION_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[9]/td[2]/span/font";
+	
+	/**
+	 * Four Years ago Oscillation
+	 */
+	public static final String FOUR_YEARS_AGO_OSCILATION_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[10]/td[2]/span/font";
+	
+	/**
+	 * Five Years ago Oscillation
+	 */
+	public static final String FIVE_YEARS_AGO_OSCILATION_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[11]/td[2]/span/font";
+	
+	
+}
