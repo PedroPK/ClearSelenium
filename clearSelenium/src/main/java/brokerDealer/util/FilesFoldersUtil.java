67d253b34413fdf9840e1d6fe48a18c1202a1b04
pedropk <pedropk@gmail.com>
2019-11-03 18:17:19 -0300

    Organizing Imports

--- clearSelenium/src/main/java/brokerDealer/util/FilesFoldersUtil.java
+++ clearSelenium/src/main/java/brokerDealer/util/FilesFoldersUtil.java
@@ -2,17 +2,8 @@ package brokerDealer.util;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
-
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class FilesFoldersUtil {
 	
