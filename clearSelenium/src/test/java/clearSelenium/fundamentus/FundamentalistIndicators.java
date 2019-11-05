8507c4260d8a5282e5de883ec71d40f4b5e588a4
pedro.carlos.santos <pedro.carlos.santos@CPX-6BIIS9Q7KYX.dir.svc.accenture.com>
2019-11-05 12:11:36 -0300

    Done the Modeling of Fundamentalist Indicators

--- clearSelenium/src/test/java/clearSelenium/fundamentus/FundamentalistIndicators.java
+++ clearSelenium/src/test/java/clearSelenium/fundamentus/FundamentalistIndicators.java
@@ -6,17 +6,14 @@ import java.math.BigDecimal;
 
 import org.openqa.selenium.WebElement;
 
+import lombok.Data;
+
+@Data
 public class FundamentalistIndicators {
 	
-	
-	
 	/************************************************************************/
-	 
 	 // First Column
-	
 	/************************************************************************/
-	
-	
 	
 	/**
 	 * Preço sobre Lucro
@@ -25,7 +22,8 @@ public class FundamentalistIndicators {
 	 * O P/L é o número de anos que se levaria para reaver o capital aplicado na compra de uma ação, 
 	 * através do recebimento do lucro gerado pela empresa, considerando que esses lucros permaneçam constantes.
 	 */
-	public static final String PRICE_OVER_PROFIT_RATIO_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[2]/td[4]/span";
+	public static final		String			PRICE_OVER_PROFIT_RATIO_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[2]/td[4]/span";
+	private					BigDecimal		aPriceOverProfit;
 	
 	/**
 	 * Preço sobre Valor Patrimonial
@@ -33,7 +31,8 @@ public class FundamentalistIndicators {
 	 * Preço da ação dividido pelo Valor Patrimonial por ação. 
 	 * Informa quanto o mercado está disposto a pagar sobre o Patrimônio Líquido da empresa
 	 */
-	public static final String PRICE_OVER_BOOK_VALUE_RATIO_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[3]/td[4]/span";
+	public static final		String			PRICE_OVER_BOOK_VALUE_RATIO_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[3]/td[4]/span";
+	private					BigDecimal		aPriveOverBookValue;
 	
 	/**
 	 * Preço sobre EBIT
@@ -41,26 +40,30 @@ public class FundamentalistIndicators {
 	 * Preço da ação dividido pelo EBIT por ação. EBIT é o Lucro antes dos Impostos e Despesas Financeiras. 
 	 * É uma boa aproximação do lucro operacional da empresa.
 	 */
-	public static final String PRICE_OVER_EBIT_RATIO_XPATH				=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[4]/td[4]/span";
+	public static final		String			PRICE_OVER_EBIT_RATIO_XPATH				=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[4]/td[4]/span";
+	private					BigDecimal		aPriceOverEBIT;
 	
 	/**
 	 * Price Sales Ratio: Preço da ação dividido pela Receita Líquida por ação
 	 */
-	public static final String PRICE_SALES_RATIO_XPATH					=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[5]/td[4]/span";
+	public static final		String			PRICE_SALES_RATIO_XPATH					=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[5]/td[4]/span";
+	private					BigDecimal		aPriceSalesRatio;
 	
 	/**
 	 * Preço sobre Ativos
 	 * 
 	 * Preço da ação dividido pelos Ativos totais por ação.
 	 */
-	public static final String PRICE_OVER_ASSETS_RATIO_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[6]/td[4]/span";
+	public static final		String			PRICE_OVER_ASSETS_RATIO_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[6]/td[4]/span";
+	private					BigDecimal		aPriceOverAsset;
 	
 	/**
 	 * Preço sobre Capital de Giro
 	 * 
 	 * Preço da ação dividido pelo capital de giro por ação. Capital de giro é o Ativo Circulante menos Passivo Circulante
 	 */
-	public static final String PRICE_OVER_WORKING_CAPITAL_RATIO_XPATH	=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[7]/td[4]/span";
+	public static final		String			PRICE_OVER_WORKING_CAPITAL_RATIO_XPATH	=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[7]/td[4]/span";
+	private					BigDecimal		aPriveOverWorkingCapital;
 	
 	/**
 	 * Preço sobre o Ativo Circulante Líquido
@@ -69,19 +72,22 @@ public class FundamentalistIndicators {
 	 * Ativo Circ. Líq. é obtido subtraindo os ativos circulantes pelas dívidas de curto e longo prazo, ou seja, 
 	 * após o pagamento de todas as dívidas, quanto sobraria dos ativos mais líquidos da empresa (caixa, estoque, etc)
 	 */
-	public static final String PRICE_OVER_NET_CURRENT_ASSET_RATIO_XPATH	=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[8]/td[4]/span";
+	public static final		String			PRICE_OVER_NET_CURRENT_ASSET_RATIO_XPATH	=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[8]/td[4]/span";
+	private					BigDecimal		aPriveOverNetCurrentAsset;
 	
 	/**
 	 * Dividend Yield
 	 * 
 	 * Dividendo pago por ação dividido pelo preço da ação. É o rendimento gerado para o dono da ação pelo pagamento de dividendos.
 	 */
-	public static final String DIVIDEND_YIELD_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[9]/td[4]/span";
+	public static final		String			DIVIDEND_YIELD_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[9]/td[4]/span";
+	private					BigDecimal		aDividendYield;
 	
 	/**
 	 * Valor da Firma (Enterprise Value dividido pelo EBIT.
 	 */
-	public static final String ENTERPRISE_VALUE_OVER_EBIT_RATIO_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[10]/td[4]/span";
+	public static final		String			ENTERPRISE_VALUE_OVER_EBIT_RATIO_XPATH		=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[10]/td[4]/span";
+	private					BigDecimal		aEnterpriseValueOverEBIT;
 	
 	/**
 	 * Giro do Ativo
@@ -89,39 +95,40 @@ public class FundamentalistIndicators {
 	 * Receita Líquida dividido por Ativos Totais. 
 	 * Indica a eficiência com a qual a empresa usa seus ativos para gerar vendas
 	 */
-	public static final String ASSET_TURNOVER_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[11]/td[4]/span";
+	public static final		String			ASSET_TURNOVER_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[11]/td[4]/span";
+	private					BigDecimal		aAssetTurnover;
 	
 	/**
 	 * Crescimento da Receita Líquida nos últimos 5 anos
 	 */
-	public static final String INCOME_GROWTH_LAST_5_YEARS_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[12]/td[4]/span";
-	
+	public static final		String			INCOME_GROWTH_LAST_5_YEARS_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[12]/td[4]/span";
+	private					BigDecimal		aIncomeGrowthLast5Years;
 	
 	
 	/************************************************************************/
-	 
 	 // Second Column
-	
 	/************************************************************************/
 	
 	
 	/**
 	 * Lucro por Ação
 	 */
-	public static final String PROFIT_PER_ACTION_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[2]/td[6]/span";
+	public static final		String			PROFIT_PER_ACTION_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[2]/td[6]/span";
+	private					BigDecimal		aProfitPerAction;
 	
 	/**
 	 * Valor Patrimonial por Ação: Valor do Patrimônio Líquido dividido pelo número total de ações.
 	 */
-	public static final String BOOK_VALUE_OVER_STOCKS_XPATH				=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[3]/td[6]/span";
+	public static final		String			BOOK_VALUE_OVER_STOCKS_XPATH				=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[3]/td[6]/span";
+	private					BigDecimal		aBookValueOverStocks;
 	
 	/**
 	 * Margem Bruta
 	 * 
 	 * Lucro Bruto dividido pela Receita Líquida: Indica a porcentagem de cada R$1 de venda que sobrou após o custo dos produtos/serviços vendidos
 	 */
-	public static final String GROSS_MARGIN_XPATH							=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[4]/td[6]/span";
-	
+	public static final		String			GROSS_MARGIN_XPATH							=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[4]/td[6]/span";
+	private					BigDecimal		aGrossMargin;
 	/**
 	 * Margem EBIT
 	 * 
@@ -131,8 +138,8 @@ public class FundamentalistIndicators {
 	 * 
 	 * Operational Margin		-		Operation income margin		-		Operating Profit Margin		-		Return of Sales (ROS)
 	 */
-	public static final String EBIT_MARGIN_XPATH							=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[5]/td[6]/span";
-	
+	public static final		String			EBIT_MARGIN_XPATH							=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[5]/td[6]/span";
+	private					BigDecimal		aEbitMargin;
 	/**
 	 * Margem Líquida
 	 * 
@@ -140,22 +147,24 @@ public class FundamentalistIndicators {
 	 * 
 	 * Profit Margin		-		Net Margin		-		Net Profit Margin		-		Net Profit Ratio
 	 */
-	public static final String NET_MARGIN_XPATH							=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[6]/td[6]/span";
+	public static final		String			NET_MARGIN_XPATH							=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[6]/td[6]/span";
+	private					BigDecimal		aNetMargin;
 	
 	/**
 	 * Return over Invested Capital
 	 * 
 	 * Retorno sobre o Capital Investido: Calculado dividindo-se o EBIT por (Ativos - Fornecedores - Caixa). Informa o retorno que a empresa consegue sobre o capital total aplicado.
 	 */
-	public static final String RETURN_OVER_INVESTED_CAPITAL_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[8]/td[6]/span";
+	public static final		String			RETURN_OVER_INVESTED_CAPITAL_XPATH			=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[8]/td[6]/span";
+	private					BigDecimal		aReturnOrverInvestedCapital;
 	
 	/**
 	 * ROE - Return on Equity
 	 * 
 	 * Retorno sobre o Patrimônio Líquido: Lucro líquido dividido pelo Patrimônio Líquido
 	 */
-	public static final String RETURN_ON_EQUITY_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[9]/td[6]/span";
-	
+	public static final		String			RETURN_ON_EQUITY_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[9]/td[6]/span";
+	private					BigDecimal		aReturnOnEquity;
 	/**
 	 * Liquidez Corrente
 	 * 
@@ -163,7 +172,8 @@ public class FundamentalistIndicators {
 	 * 
 	 *  Current Liquity		-		Current Ratio		-		Liquidity Ratio
 	 */
-	public static final String CURRENT_LIQUIDITY_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[10]/td[6]/span";
+	public static final		String			CURRENT_LIQUIDITY_XPATH						=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[10]/td[6]/span";
+	private					BigDecimal		aCurrentLiquidity;
 	
 	/**
 	 * Dívida Bruta sobre Patrimônio Líquido
@@ -172,22 +182,9 @@ public class FundamentalistIndicators {
 	 * 
 	 * Debt to Equity Ratio
 	 */
-	public static final String DEBT_TO_EQUITY_RATIO_XPATH					=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[11]/td[6]/span";
+	public static final		String			DEBT_TO_EQUITY_RATIO_XPATH					=	"/html/body/div[1]/div[2]/table[3]/tbody/tr[11]/td[6]/span";
+	private					BigDecimal		aDebtToEquityRatio;
 	
-	private BigDecimal aPriceOverProfit;
-	private BigDecimal aPriveOverBookValue;
-	private BigDecimal aPriceOverEBIT;
-	
-	private BigDecimal aPriceSalesRatio;
-	private BigDecimal aPriceOverStocks;
-	private BigDecimal aPriceOverAsset;
-	
-	private BigDecimal aPriveOverWorkingCapital;
-	private BigDecimal aPriveOverNetCurrentAsset;
-	private BigDecimal aDividendYield;
-	private BigDecimal aEenterpriseValueOverEBIT;
-	private BigDecimal aAssetTurnover;
-	private BigDecimal aIncomeGrowthLast5Years;
 	
 	public static String getDividendYieldTextValue() {
 		WebElement dividendYieldWebElement = getElementByXPath(FundamentalistIndicators.DIVIDEND_YIELD_XPATH);
