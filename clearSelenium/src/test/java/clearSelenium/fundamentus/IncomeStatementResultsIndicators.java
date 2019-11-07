e62a0cc9cd98e9f4c465e2e1e284d8824c713e9c
pedro.carlos.santos <pedro.carlos.santos@CPX-6BIIS9Q7KYX.dir.svc.accenture.com>
2019-11-07 10:24:05 -0300

    Methods to get Values from HTML Page with Selenium WebDriver by XPATH

--- clearSelenium/src/test/java/clearSelenium/fundamentus/IncomeStatementResultsIndicators.java
+++ clearSelenium/src/test/java/clearSelenium/fundamentus/IncomeStatementResultsIndicators.java
@@ -1,6 +1,10 @@
 package clearSelenium.fundamentus;
 
+import static clearSelenium.SeleniumUtils.getElementByXPath;
+
 import java.math.BigDecimal;
+
+import org.openqa.selenium.WebElement;
 
 import lombok.Data;
 
@@ -13,11 +17,21 @@ public class IncomeStatementResultsIndicators {
 	public static final		String		NET_INCOME_LAST_12_MONTHS_XPATH				=	"/html/body/div[1]/div[2]/table[5]/tbody/tr[3]/td[2]/span";
 	private					BigDecimal	aNEtIncomeLast12Months;
 	
+	public static String geNetIncomeLast12Months() {
+		WebElement dividendYieldWebElement = getElementByXPath(NET_INCOME_LAST_12_MONTHS_XPATH);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * EBIT dos Últimos 12 Meses		-		EBIT from Last 12 Months
 	 */
 	public static final		String		EBIT_LAST_12_MONTHS_XPATH					=	"/html/body/div[1]/div[2]/table[5]/tbody/tr[4]/td[2]/span";
 	private					BigDecimal	aEbitLast12Months;
+	
+	public static String geEbitLast12Months() {
+		WebElement dividendYieldWebElement = getElementByXPath(EBIT_LAST_12_MONTHS_XPATH);
+		return dividendYieldWebElement.getText();
+	}
 	
 	/**
 	 * Lucro Líquido dos Últimos 12 Meses		-		Net Profit from Last 12 Months
@@ -25,6 +39,10 @@ public class IncomeStatementResultsIndicators {
 	public static final		String		NET_PROFIT_LAST_12_MONTHS_XPATH				=	"/html/body/div[1]/div[2]/table[5]/tbody/tr[5]/td[2]/span";
 	private 				BigDecimal	aNetProfitLast12Months;
 	
+	public static String geNetProfitLast12Months() {
+		WebElement dividendYieldWebElement = getElementByXPath(NET_PROFIT_LAST_12_MONTHS_XPATH);
+		return dividendYieldWebElement.getText();
+	}
 	
 	/**
 	 * Receita Líquida dos Últimos 3 Meses		-		Net Income from Last 3 Months
@@ -32,16 +50,31 @@ public class IncomeStatementResultsIndicators {
 	public static final		String		NET_INCOME_LAST_03_MONTHS_XPATH				=	"/html/body/div[1]/div[2]/table[5]/tbody/tr[3]/td[4]/span";
 	private 				BigDecimal	aNetIncomeLast03Months;
 	
+	public static String geNetIncomeLast03Months() {
+		WebElement dividendYieldWebElement = getElementByXPath(NET_INCOME_LAST_03_MONTHS_XPATH);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * EBIT dos Últimos 03 Meses		-		EBIT from Last 03 Months
 	 */
 	public static final		String		EBIT_LAST_03_MONTHS_XPATH					=	"/html/body/div[1]/div[2]/table[5]/tbody/tr[4]/td[4]/span";
 	private 				BigDecimal	aEbitLast03Months;
 	
+	public static String geEbitLast03Months() {
+		WebElement dividendYieldWebElement = getElementByXPath(EBIT_LAST_03_MONTHS_XPATH);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * Lucro Líquido dos Últimos 03 Meses		-		Net Profit from Last 03 Months
 	 */
 	public static final		String		NET_PROFIT_LAST_03_MONTHS_XPATH				=	"/html/body/div[1]/div[2]/table[5]/tbody/tr[5]/td[4]/span";
 	private 				BigDecimal	aNetProfitLast03Months;
+	
+	public static String geNetProfitLast03Months() {
+		WebElement dividendYieldWebElement = getElementByXPath(NET_PROFIT_LAST_03_MONTHS_XPATH);
+		return dividendYieldWebElement.getText();
+	}
 	
 }
