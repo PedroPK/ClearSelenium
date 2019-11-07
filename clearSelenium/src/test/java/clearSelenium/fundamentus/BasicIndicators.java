ee250f7c47edf0819b84734b8716ebf42e0a3b71
pedro.carlos.santos <pedro.carlos.santos@CPX-6BIIS9Q7KYX.dir.svc.accenture.com>
2019-11-07 10:39:49 -0300

    Methods do get Values from HTML page, with Selenium WebDriver, by XPATH.

--- clearSelenium/src/test/java/clearSelenium/fundamentus/BasicIndicators.java
+++ clearSelenium/src/test/java/clearSelenium/fundamentus/BasicIndicators.java
@@ -1,6 +1,10 @@
 package clearSelenium.fundamentus;
 
+import static clearSelenium.SeleniumUtils.getElementByXPath;
+
 import java.math.BigDecimal;
+
+import org.openqa.selenium.WebElement;
 
 import lombok.Data;
 
@@ -13,11 +17,21 @@ public class BasicIndicators {
 	public static final		String		TICKER							=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[1]/td[2]/span";
 	private					BigDecimal	aTicker;
 	
+	public static String geTicker() {
+		WebElement dividendYieldWebElement = getElementByXPath(TICKER);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * ON = Ordinária, PN = Preferencial, PNA = Pref. tipo A, etc
 	 */
 	public static final		String		TYPE							=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[2]/td[2]/span";
 	private					BigDecimal	aType;
+	
+	public static String geType() {
+		WebElement dividendYieldWebElement = getElementByXPath(TYPE);
+		return dividendYieldWebElement.getText();
+	}
 	
 	/**
 	 * Nome comercial da empresa.
@@ -25,11 +39,21 @@ public class BasicIndicators {
 	public static final		String		COMPANY_NAME					=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[3]/td[2]/span";
 	private					BigDecimal	aCompanyName;
 	
+	public static String geCompany() {
+		WebElement dividendYieldWebElement = getElementByXPath(COMPANY_NAME);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * Classificação setorial
 	 */
 	public static final		String		SECTOR							=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[4]/td[2]/span/a";
 	private					BigDecimal	aSector;
+	
+	public static String geSector() {
+		WebElement dividendYieldWebElement = getElementByXPath(SECTOR);
+		return dividendYieldWebElement.getText();
+	}
 	
 	/**
 	 * Classificação por segmento de atuação.
@@ -37,11 +61,21 @@ public class BasicIndicators {
 	public static final		String		SUB_SECTOR						=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[5]/td[2]/span/a";
 	private					BigDecimal	aSubSector;
 	
+	public static String geSubSector() {
+		WebElement dividendYieldWebElement = getElementByXPath(SUB_SECTOR);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * Cotação de fechamento da ação no último pregão
 	 */
 	public static final		String		PRICE							=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[1]/td[4]/span";
 	private					BigDecimal	aPrice;
+	
+	public static String gePrice() {
+		WebElement dividendYieldWebElement = getElementByXPath(PRICE);
+		return dividendYieldWebElement.getText();
+	}
 	
 	/**
 	 * Data do último pregão em  que o ativo foi negociado
@@ -49,11 +83,21 @@ public class BasicIndicators {
 	public static final		String		DATE_PRICE						=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[2]/td[4]/span";
 	private					BigDecimal	aDatePrice;
 	
+	public static String geDatePrice() {
+		WebElement dividendYieldWebElement = getElementByXPath(DATE_PRICE);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * Menor cotação da ação nos últimos 12 meses.
 	 */
 	public static final		String		MINIMUM_PRICE_LAST_52_WEEKS		=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[3]/td[4]/span";
 	private					BigDecimal	aMinimumPriceLast32Weeks;
+	
+	public static String geMinimumPriceLast52Weeks() {
+		WebElement dividendYieldWebElement = getElementByXPath(MINIMUM_PRICE_LAST_52_WEEKS);
+		return dividendYieldWebElement.getText();
+	}
 	
 	/**
 	 * Maior cotação da ação nos últimos 12 meses
@@ -61,10 +105,21 @@ public class BasicIndicators {
 	public static final		String		MAXIMUM_PRICE_LAST_52_WEEKS		=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[4]/td[4]/span";
 	private					BigDecimal	aMaximumPriceLast32Weeks;
 	
+	public static String geMaximumPriceLast52Weeks() {
+		WebElement dividendYieldWebElement = getElementByXPath(MAXIMUM_PRICE_LAST_52_WEEKS);
+		return dividendYieldWebElement.getText();
+	}
+	
 	/**
 	 * Volume médio de negociação da ação nos últimos 2 meses (R$)
 	 */
 	public static final		String		AVERAGE_VOLUME_LAST_2_MONTHS	=	"/html/body/div[1]/div[2]/table[1]/tbody/tr[5]/td[4]/span";
 	private					BigDecimal	aAverageVolumeLast32Weeks;
+	
+
+	public static String geMAverageVolumeLast52Weeks() {
+		WebElement dividendYieldWebElement = getElementByXPath(AVERAGE_VOLUME_LAST_2_MONTHS);
+		return dividendYieldWebElement.getText();
+	}
 	
 }
